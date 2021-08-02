991be78a3c29cc38934c7569edde5e668408f398
fuubi <fuubi@protonmail.ch>
2021-08-02 08:16:48 +0200

    Fix: To be Signed Array Buffer.
    (#2)
    Update the JWT class fields.

--- src/crypto/Jwt.ts
+++ src/crypto/Jwt.ts
@@ -3,19 +3,18 @@ import {Convert} from "pvtsutils";
 export class Jwt {
     public header: any
     public payload: any
-    public mac: ArrayBuffer
+    public signature: ArrayBuffer
+    public toBeSigned: ArrayBuffer
 
-    public signedContent: ArrayBuffer
-
-    constructor(jwt: string) {
-        const parts = jwt.split(".")
-        if (parts.length !== 3) {
+    constructor(public raw: string) {
+        const {0: header, 1: payload, 2: signature} = raw.split(".")
+        if (!header || !payload || !signature) {
             throw Error("Only Json Web Token (JWT) with 3 parts are supported. See: https://datatracker.ietf.org/doc/html/rfc7519#section-3.1")
         }
-        this.header = JSON.parse(atob(parts[0]));
-        this.payload = JSON.parse(atob(parts[1]));
-        this.mac = Convert.FromBase64Url(parts[2]);
-        this.signedContent = Convert.FromBase64Url(parts[1])
+        this.header = JSON.parse(atob(header));
+        this.payload = JSON.parse(atob(payload));
+        this.signature = Convert.FromBase64Url(signature);
+        this.toBeSigned = Buffer.from(`${header}.${payload}`,"ascii");
     }
 
 }
--- test/unit/crypto/Jwt.test.ts
+++ test/unit/crypto/Jwt.test.ts
@@ -1,14 +1,29 @@
-import {CH_KEYS_UPDATE_LIST_JWT} from "../../../src/data/ch-keys/updatelistJwt";
-import {Jwt} from "../../../src/crypto/Jwt";
+import {CH_KEYS_UPDATE_LIST_JWT} from "../../../src";
+import {Jwt} from "../../../src";
 
 describe("Test the jwt", ( ) => {
-    test("Should parse the JWT", async () =>{
+    test("Should parse CH key update list jwt", async () =>{
             const jwt = new Jwt(CH_KEYS_UPDATE_LIST_JWT)
         expect(jwt.header.alg).toBe("RS256")
         expect(jwt.header.x5c.length).toBe(2)
 
         expect(jwt.payload.certs.length).toBe(131)
-        expect(jwt.mac instanceof ArrayBuffer).toBeTruthy()
+        expect(jwt.signature instanceof ArrayBuffer).toBeTruthy()
         }
-    )}
+    )
+    test("Should parse the example token from RFC7519.", async () =>{
+            const jwt = new Jwt("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk")
+            expect(jwt.header.alg).toBe("HS256")
+            expect(jwt.header.typ).toBe("JWT")
+
+            expect(jwt.payload.iss).toBe("joe")
+            expect(jwt.signature instanceof ArrayBuffer).toBeTruthy()
+            }
+        )
+
+        test("Should throw a error for incompatible jwt", async () =>{
+                expect(() => new Jwt("eyJhbGciOiJSUzI1NiJ9")).toThrowError()
+            }
+        )
+    }
 )
